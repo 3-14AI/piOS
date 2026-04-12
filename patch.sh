7de353a42f84c573d506f0f0f40c3cef26ccf54f
pimenovdv <25670278+pimenovdv@users.noreply.github.com>
2026-04-12 20:07:39 +0000

    Cleanup garbage files and consolidate reports

--- patch.sh
+++
@@ -1,2 +0,0 @@
-sed -i '/impl MockDevice {/i impl Default for MockDevice {\n    fn default() -> Self {\n        Self::new()\n    }\n}\n' userland/services/net_stack/src/lib.rs
-sed -i '/impl WasmNetStack {/i impl Default for WasmNetStack {\n    fn default() -> Self {\n        Self::new()\n    }\n}\n' userland/services/net_stack/src/lib.rs
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cargo install cargo-tarpaulin
-cargo tarpaulin --workspace --engine llvm --fail-under 87 --exclude-files 'tools/verus/*' --out Xml
